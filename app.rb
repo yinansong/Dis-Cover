5a2d7111982e17eabe3f2b52e20920425dbb53b9
yinansong <yinan@yinansong.com>
2014-09-05 11:42:15 -0400

    set up the scaffold

---
+++ app.rb
@@ -0,0 +1,31 @@
+require 'sinatra/base'
+
+class App < Sinatra::Base
+
+  ########################
+  # Configuration
+  ########################
+
+  configure do
+    enable :logging
+    enable :method_override
+    enable :sessions
+  end
+
+  before do
+    logger.info "Request Headers: #{headers}"
+    logger.warn "Params: #{params}"
+  end
+
+  after do
+    logger.info "Response Headers: #{response.headers}"
+  end
+
+  ########################
+  # Routes
+  ########################
+
+  get('/') do
+    render(:erb, :index)
+  end
+end
